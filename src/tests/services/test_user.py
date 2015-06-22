e2cc3c243bc0307cd7e1ff35f64830e2d487e1ac
Farzad Ghanei <farzad.ghanei@gmail.com>
2015-06-22 23:16:41 +0800

    Fixes tests for user service
    We should pass in the secret key to auth verification as well.

--- src/tests/services/test_user.py
+++ src/tests/services/test_user.py
@@ -12,22 +12,24 @@ class TestUser(unittest.TestCase):
         user = User(email='test@example.org', name='myname', surname='is test', id=1)
         mock_verify_auth.return_value = user
         logger = mock.MagicMock()
-        config = {'SECRET_KEY': 'this key is secret'}
+        secret_key = 'this key is secret'
+        config = {'SECRET_KEY': secret_key}
         service = UserService(logger, config)
         token = service.generate_auth_token(user)
         auth_user = service.authenticate(token)
-        mock_verify_auth.assert_called_with(token)
+        mock_verify_auth.assert_called_with(secret_key, token)
         self.assertEqual(auth_user, user)
 
     @mock.patch.object(User, "verify_auth_token")
     @mock.patch("project.models.user.db.session")
-    def test_authenticate_invalid_token_returns_none(self, mock_session, mock_verify_auth):
+    def test_authenticate_invalid_token_returns_none(self, mock_db_session, mock_verify_auth):
         mock_verify_auth.return_value = None
         logger = mock.MagicMock()
-        config = {'SECRET_KEY': 'this key is secret'}
+        secret_key = 'this key is secret'
+        config = {'SECRET_KEY': secret_key}
         service = UserService(logger, config)
         user = service.authenticate('this is a token')
-        mock_verify_auth.assert_called_with('this is a token')
+        mock_verify_auth.assert_called_with(secret_key, 'this is a token')
         self.assertIsNone(user)
 
 if __name__ == '__main__':
